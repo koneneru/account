0388323d628e10b7531092178e4b8fb7b43d8eaf
k0neru <koneneru@gmail.com>
2026-06-15 10:54:09 +0400

    feat[internal/config]: add config loader and .env.example

---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+
+	"github.com/caarlos0/env/v10"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServiceName string `env:"SERVICE_NAME" required:"true" default:"account-service"`
+	AppEnv      string `env:"APP_ENV" required:"true" default:"development"`
+	Host        string `env:"HTTP_HOST" required:"true" default:"localhost"`
+	Port        int    `env:"HTTP_PORT" required:"true" default:"9000"`
+	LogLevel    string `env:"LOG_LEVEL" required:"true" default:"info"`
+}
+
+func Load() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found, using system environment variables")
+	}
+
+	cfg := &Config{}
+	err := env.Parse(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
